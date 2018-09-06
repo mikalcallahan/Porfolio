c1df0aa5f52434829c53ea78768f43bb61f58ae0
Mikal Callahan <mikal.callahan@icloud.com>
2018-09-06 11:38:08 -0700

    + google analytics (sorry not trying to be creepy) + config.js for api keys and whatnot

--- src/router.js
+++ src/router.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import Home from './views/Home.vue';
+import VueAnalytics from 'vue-analytics';
 import About from './views/About.vue';
 // import Projects from './views/Projects.vue';
 // import Contact from './views/Contact.vue';
 import ComingSoon from './views/ComingSoon.vue';
 
 Vue.use(Router);
+Vue.use(VueAnalytics, {
+  id: '@/config.js',
+  Router,
+});
 
 export default new Router({
   routes: [
