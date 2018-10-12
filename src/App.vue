a6f3d6dcd9e1fb3bacb6087a9e6df84b03498a0e
Mikal Callahan <mikal.callahan@icloud.com>
2018-10-12 13:47:00 -0700

    + lazy-load header and footer + more img compression

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,10 @@
 import '@/styles/app.scss';
 
 // import Components
-import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
+const Header = () => import('@/components/Header.vue');
+const Footer = () => import('@/components/Footer.vue');
+// import Header from '@/components/Header.vue';
+// import Footer from '@/components/Footer.vue';
 
 // Export app
 export default {
