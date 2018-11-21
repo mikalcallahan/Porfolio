7dff6dd06e2b367860a8c733fd858bf174c34dc2
Mikal Callahan <mikal.callahan@icloud.com>
2018-11-21 13:05:13 -0800

    More Layout of About Page Modified fonts/color Pre-flexbox as main config

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,33 +1,44 @@
 <!-- About Page -->
 <template>
-  <div class="about">
-    <h1 class='title'>Mikal Callahan</h1>
-    <h3 class='subtitle'>Software development, Ui/Ux design.</h3>
-    <div class='about en'>
-      <!--
-      <p class='about__description'>
+  <section class='about'>
+  <div class='about about--en'>
+    <div class='about__intro'>
+      <h1 class='title'>Mikal Callahan</h1>
+      <h3 class='subtitle'>Software development, Ui/Ux design.</h3>
+    </div>
+    <p class='about__description description--one'>
       I am a software developer and ui/ux designer in love with the process of designing
       and developing user experiences.
       I create full-stack solutions to web oriented needs, from e-commerce to web apps.
+    </p>
+    <ul class='about__services'>
+    <li v-for='service in services'>
+      <h2 id='service__name'>{{ service.name }}</h2>
+      <h4 id='service__desc'>{{ service.desc }}</h4>
+    </li>
+    </ul>
+    <p class='about__description description--two'>
       Each partnership is different and the values and needs of those I work with
       always come first.
       After our goals have been defined I develop a roadmap for the project,
       iterate through designs, then development.
+    </p>
+    <p class='about__description description--three'>
       I have had the pleasure of working with wonderful people including Bettina Heinz,
       Adaptive Web Studio, Heavenly Gift Baskets, and Tiled.
-      </p>
-      <img class='about__photo' src=''/>
-      <ul class='about__services'>
-      </ul>
-      -->
-    </div>
-    <div class='about fr'></div>
+    </p>
+    <img class='about__photo' src='@/assets/mikal.jpg'/>
   </div>
+  <div class='about about--fr'>
+
+  </div>
+  </section>
 </template>
 
 <script>
 // import styles
 import '@/styles/navbar.scss';
+import '@/styles/about.scss';
 
 // more or less default export
 export default {
@@ -50,7 +61,7 @@ export default {
         },
         {
           name: 'Consulting',
-          desc: 'If you just want some advince',
+          desc: 'If you just want some advice',
         },
       ],
       tools: [
